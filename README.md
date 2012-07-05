2abd5d07e3e51268353cadbef35f3be6ed45e9f1
Justin Kramer <jkkramer@gmail.com>
2012-07-05 13:48:05 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# util
+
+General-purpose Clojure utility functions.
+
+## Usage
+
+Leiningen coordinate:
+
+    [com.jkkramer/util "1.0.0-SNAPSHOT"]
+
+## License
+
+Copyright © 2012 Justin Kramer
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject com.jkkramer/util "1.0.0-SNAPSHOT"
+  :description "Justin Kramer's general-purpose utility libraries"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
