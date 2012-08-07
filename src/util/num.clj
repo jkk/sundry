2ef99b5a4d93d70e6d8613c528a74c5ac0fb74bf
Justin Kramer <jkkramer@gmail.com>
2012-08-07 18:00:32 -0400

    parse-long

--- src/util/num.clj
+++ src/util/num.clj
@@ -5,6 +5,11 @@
     (int x)
     (try (Integer/valueOf x) (catch Exception _ default))))
 
+(defn parse-long [x & [default]]
+  (if (number? x)
+    (int x)
+    (try (Long/valueOf x) (catch Exception _ default))))
+
 (defn parse-double [x & [default]]
   (if (number? x)
     (double x)
--- test/util/num_test.clj
+++ test/util/num_test.clj
@@ -9,6 +9,12 @@
   (is (= 123 (parse-int "0123")))
   (is (= 123 (parse-int 123.0))))
 
+(deftest test-parse-long
+  (is (= 123 (parse-long "123")))
+  (is (nil? (parse-long "123.0")))
+  (is (= 123 (parse-long "0123")))
+  (is (= 123 (parse-long 123.0))))
+
 (deftest test-parse-double
   (is (= 123.0 (parse-double "123")))
   (is (= 123.0 (parse-double "123.0")))
