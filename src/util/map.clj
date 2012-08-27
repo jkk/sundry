5ae2dcf723f35514c1f48ead699a80a787df3d7c
Justin Kramer <jkkramer@gmail.com>
2012-08-27 13:45:54 -0400

    assoc & update fns

--- src/util/map.clj
+++ src/util/map.clj
@@ -10,10 +10,46 @@
        (assoc m1 k v)))
    m1 m2))
 
-(defn assoc-unless [pred m & kvs]
+#_(defn assoc-unless [pred m & kvs]
   (reduce
    (fn [m [k v]]
      (if (pred v)
        m
        (assoc m k v)))
    m (partition 2 kvs)))
+
+(defn assoc-if [m pred & kvs]
+  (reduce
+   (fn [m [k v]]
+     (if (pred m k v)
+       (assoc m k v)
+       m))
+   m
+   (partition 2 kvs)))
+
+(defn assoc-present [m & kvs]
+  (apply assoc-if m
+         (fn [m k _]
+           (contains? m k))
+         kvs))
+
+(defn assoc-absent [m & kvs]
+  (apply assoc-if m
+         (fn [m k _]
+           (not (contains? m k)))
+         kvs))
+
+(defn update-if [m pred k f & args]
+  (if (pred m k)
+    (apply update-in m [k] f args)
+    m))
+
+(defn update-present [m k f & args]
+  (apply update-if m (fn [m k]
+                       (contains? m k))
+         k f args))
+
+(defn update-absent [m k f & args]
+  (apply update-if m (fn [m k]
+                       (not (contains? m k)))
+         k f args))
