9e43f7927f973117240d038a848bfd6a9b817323
Justin Kramer <jkkramer@gmail.com>
2012-10-15 17:46:59 -0400

    shift-when

--- src/sundry/core.clj
+++ src/sundry/core.clj
@@ -3,6 +3,11 @@
 (defn mapply [f & args]
   (apply f (apply concat (butlast args) (last args))))
 
+(defn shift-when [pred [x & xs :as coll]]
+  (if (pred x)
+    [x xs]
+    [nil coll]))
+
 (defn full-name [x]
   (str (when-let [ns (namespace x)]
          (str (namespace x) "/"))
