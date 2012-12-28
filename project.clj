92c7d52b01cb02f54e7209970386161084f20ad2
Justin Kramer <jkkramer@gmail.com>
2012-12-28 13:16:16 -0500

    0.4.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sundry "0.3.0"
+(defproject sundry "0.4.0-SNAPSHOT"
   :description "Collection of miscellaneous utility functions"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
